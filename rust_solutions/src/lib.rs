0fceda1adefe8d9762c4153fb6d3630e3446042a
plasticuproject <19690649+plasticuproject@users.noreply.github.com>
2024-08-25 21:04:57 -0400

    Added edges struct

--- rust_solutions/src/lib.rs
+++ rust_solutions/src/lib.rs
@@ -1,5 +1,6 @@
 mod connected_components_count;
 mod depth_first_and_breadth_first_traversal;
+mod edges;
 mod graph;
 mod grid;
 mod has_path;
@@ -7,5 +8,6 @@ mod island_count;
 mod largest_component;
 mod minimum_island;
 
+pub use edges::Edges;
 pub use graph::Graph;
 pub use grid::Grid;
